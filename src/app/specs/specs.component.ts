e2c5a3557216b4b16b4e79cf698a962ffd2d06af
R P Buvan Varun <buvanvarun.acad@gmail.com>
2020-08-03 22:19:30 +0530

    made changes

--- src/app/specs/specs.component.ts
+++ src/app/specs/specs.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-specs',
   templateUrl: './specs.component.html',
-  styleUrls: ['./specs.component.css']
+  styleUrls: ['./specs.component.css'],
 })
 export class SpecsComponent implements OnInit {
+  features: string[] = [
+    `Over the air updates
+    & remote diagnostics.`,
+    `Theft detection
+  notifications.`,
+    `2 Years maintenance &
+  warranty on motor &
+  battery.`,
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
